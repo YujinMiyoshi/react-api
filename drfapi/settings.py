bad67ef46fafefc22b7b9820c404ef56da949c88
yujin <deus1@outlook.jp>
2022-08-26 23:59:33 +0900

    databases

--- drfapi/settings.py
+++ drfapi/settings.py
@@ -86,14 +86,14 @@ WSGI_APPLICATION = 'drfapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-"""
+
 DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
+
 
 
 # Password validation
